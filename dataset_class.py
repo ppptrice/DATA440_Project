47f5263923fbf8f6d5e23d346e77819129bd2021
ppptrice <129793280+ppptrice@users.noreply.github.com>
2023-12-08 02:03:32 -0500

    streamlit updates
    - more functionality
    - negative binomial dist added

--- dataset_class.py
+++ dataset_class.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # class to test out data generation methods
 class dataset:
@@ -39,7 +40,6 @@ class dataset:
         elif cols != None:
             return self.data_summary[cols]
     
-    
     def get_dataframe(self):
         return self.data
     
@@ -53,7 +53,8 @@ class dataset:
                       col = None, 
                       n_records = 126, 
                       compare_dists = True,
-                      distribution = 'poisson'
+                      distribution = 'poisson',
+                      prob = 0.5
                       ):
         '''
         Simulates a histogram of levels of a specific cytokine based on the original data. 
@@ -64,6 +65,8 @@ class dataset:
             x = np.random.poisson(lam = self.get_means(cols = col), size = n_records)
         elif distribution == 'normal':
             x = np.random.normal(loc = self.get_means(cols = col), scale = self.get_variance(cols = col), size = n_records)
+        elif distribution == 'negative binomial':
+            x = np.random.negative_binomial(n=self.get_means(cols = col), p = prob, size = n_records)
             
         if compare_dists == False:
             fig = plt.figure(figsize=(8,4))
@@ -74,19 +77,27 @@ class dataset:
         else:
             f = plt.figure(figsize=(8,8))
             f,[data_hist, new_hist] = plt.subplots(1,2, sharex=True)
-            data_hist.hist(x = self.data[col])
-            new_hist.hist(x = x)
+            data_hist.hist(x = self.data[col], linewidth = 1, color = 'skyblue', bins = 15,
+                           ec = 'white')
+            new_hist.hist(x = x, linewidth = 1, color = 'firebrick',
+                          ec = 'white')
+            
+            data_hist.tick_params(pad = 10)
+            new_hist.tick_params(pad = 10)
+            
+            [data_hist.spines[i].set_visible(False) for i in data_hist.spines]
+            [new_hist.spines[i].set_visible(False) for i in new_hist.spines]
             
             data_hist.set_title('Observed data')
             new_hist.set_title('Generated data')
         return f
     
     def generate_records(self, 
-                        cols,
-                        #add_in_place = False,
-                        n_records = 1,
-                        distribution = 'poisson',
-                        ):
+                         cols,
+                         n_records = 1,
+                         distribution = 'poisson',
+                         prob = 0.5
+                         ):
         df = pd.DataFrame()
         if distribution == 'poisson':
             for col in cols:
@@ -96,4 +107,8 @@ class dataset:
                 df[col] = np.round(np.random.normal(loc = self.get_means(cols = col), 
                                                     scale = self.get_variance(cols = col), 
                                                     size = n_records), decimals = 2)
+        elif distribution == 'negative binomial':
+            for col in cols:
+                df[col] = np.round(np.random.negative_binomial(n=self.get_means(cols = col), 
+                                                               p = prob, size = n_records), 2)
         return df
--- st_app.py
+++ st_app.py
@@ -7,7 +7,8 @@ from dataset_class import dataset
 
 # Header of the website
 st.write("""
-# Simple simulation of normal human cytokine levels
+# Simple Simulation of Normal Human Cytokine Levels 
+---
 The following tools help to coceptualize, visualize, and explore different cytokines of the immune system.
 First, what is a cytokine?
 
@@ -24,6 +25,7 @@ Below is a section of a dataframe containing many different cytokine levels of n
 The data are measured in mean fluorescence intensity (MFI).
 """)
 
+############ ENTIRE DATASET
 # Creation of a "dataset" class that allows specific functions to be called later
 cyto_dataset = dataset('norm_cytokine_data.csv')
 df = cyto_dataset.get_dataframe()
@@ -33,11 +35,14 @@ st.sidebar.header('Cytokine selection') # creates sidebar with title
 
 # Creating selection of cytokines using sidebar
 unique_cyto = df.columns.values
-selected_cyto = st.sidebar.multiselect('Cytokines used in', unique_cyto, unique_cyto)
+selected_cyto = st.sidebar.multiselect('Cytokines used in data selection', unique_cyto, unique_cyto)
 df_selected_cyto = df[selected_cyto]
 
+
+################# SELECTED CYTOKINE DATAFRAME
 # 
-st.header('Dataframe of selected cytokines')
+st.header('Dataframe of Selected Cytokines')
+st.divider()
 st.write("""The following dataframe displays all cytokines that are selected on the sidebar to the left, 
 under 'Cytokine selection'.
 """)
@@ -45,47 +50,87 @@ st.write('Current cytokines: ' + str([cyto for cyto in selected_cyto]))
 st.write('Data Dimension: ' + str(df_selected_cyto.shape[0]) + ' rows and ' + str(df_selected_cyto.shape[1]) + ' columns.')
 st.dataframe(df_selected_cyto)
 
+
+############## SUMMARY STATISTICS
 # Summary statistics dataframe generation
-st.header('Summary statistics of selected cytokines')
+st.header('Summary Statistics of Selected Cytokines')
+st.divider()
+st.write('''The following dataframe displays simple summary statistics of the data.
+They will also adjust to the selected cytokines on the sidebar under 'Cytokine selection'
+''')
 st.dataframe(cyto_dataset.get_summary(cols = selected_cyto))
 
+
+############## HISTOGRAM
 # Displays a histogram of cytokine data generated depending on the selected distribution in the sidebar
-st.header('Simulated histogram')
+st.header('Simulated Histogram')
+st.divider()
+st.write('''This visualization allows you to test out several different distributions to model 
+the specified cytokine selected on the sidebar.
+''')
 cyto_option = st.sidebar.selectbox('Histogram cytokine:', unique_cyto)
 'Selected cytokine: ', cyto_option
 
-dist_option = st.sidebar.selectbox('Type of distribution:', ['poisson', 'normal'])
+dist_option = st.sidebar.selectbox('Type of distribution:', ['poisson', 'normal', 'negative binomial'])
 'Selected distribution: ', dist_option
 
-hist_fig = cyto_dataset.generate_hist(col = cyto_option, distribution = dist_option)
+if dist_option == 'negative binomial':
+    nbinom_prob = st.sidebar.number_input(label = 'Insert the probability for the negative binomial distribution',
+                                          min_value = 0.000001, max_value = 1.0, value = 0.5, step = 0.01)
+    hist_fig = cyto_dataset.generate_hist(col = cyto_option, distribution = dist_option, prob = nbinom_prob)
+else:
+    hist_fig = cyto_dataset.generate_hist(col = cyto_option, distribution = dist_option)
 
 st.pyplot(hist_fig)
 
-if st.button('Correlation Heatmap'):
-    st.header('Correlation Heatmap')
-    df_selected_cyto.to_csv('output.csv',index=False)
-    corr_df = pd.read_csv('output.csv')
 
-    corr = corr_df.corr()
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
-    with sns.axes_style("white"):
-        f, ax = plt.subplots()
-        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    st.pyplot(f)
-    
+############ CORRELATION HEATMAP
+#if st.button('Correlation Heatmap'):
+st.header('''Correlation Heatmap
+---''')
+st.write("""This heatmap shows any possible correlations between the selected cytokines on the sidebar.
+It may be useful to discern relationships between certain cytokines (e.g. pro-inflammatory cytokines may
+be correlated with each other).
+""")
+df_selected_cyto.to_csv('output.csv',index=False)
+corr_df = pd.read_csv('output.csv')
+
+corr_nums = st.checkbox(label = 'Display correlation numbers on heatmap?')
+
+corr = corr_df.corr()
+mask = np.zeros_like(corr)
+mask[np.triu_indices_from(mask)] = True
+with sns.axes_style("white"):
+    f, ax = plt.subplots()
+    ax = sns.heatmap(corr, mask=mask, vmax=1, vmin=-1, square=True,
+                     annot=corr_nums, fmt=".2f", linewidth = .5, cmap='vlag',
+                     annot_kws={"size": 30 / np.sqrt(len(corr))})
+st.pyplot(f)
+
+
+############### GENERATED CYTOKINE DATA
 st.sidebar.header('Cytokine selection for data generation')
     
 generate_cyto = st.sidebar.multiselect('Cytokines to generate', unique_cyto, unique_cyto)
-dist_option2 = st.sidebar.selectbox('Type of distribution to generate:', ['poisson', 'normal'])
+dist_option2 = st.sidebar.selectbox('Type of distribution to generate:', ['poisson', 'normal', 'negative binomial'])
 num_records = st.sidebar.slider('Number of records to generate', 1, 500, 250)
 
-
-st.header('Cytokine dataframe generation')
+st.header('Cytokine Dataframe Generation')
+st.divider()
 'Selected distribution: ', dist_option2
+st.write('''Using the sidebar, you can generate your own data using the specified parameters!
+Below is the dataframe you created.
+''')
 #'Cytokines generated', generate_cyto
-st.dataframe(cyto_dataset.generate_records(cols = generate_cyto, distribution = dist_option2, n_records = num_records))
+if dist_option2 == 'negative binomial':
+    nbinom_prob2 = st.sidebar.number_input(label = 'Input the probability for the negative binomial distribution',
+                                          min_value = 0.000001, max_value = 1.0, value = 0.5, step = 0.01)
+    st.dataframe(cyto_dataset.generate_records(cols = generate_cyto, distribution = dist_option2, n_records = num_records, prob = nbinom_prob2))
+else:
+    st.dataframe(cyto_dataset.generate_records(cols = generate_cyto, distribution = dist_option2, n_records = num_records))
 
+
+################ removes the bottom text
 def streamlit_defaults():
     '''
     Remove some auto-generated stuff by streamlit
